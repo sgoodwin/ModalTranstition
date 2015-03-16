5bb3c409ee228043f2b51b8bbf64048b2032c44d
Samuel Goodwin <samuel@roundwallsoftware.com>
2015-03-16 21:46:40 +0100

    Setup with a basic modal transition.

---
+++ ModalTransition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ModalTransition
+//
+//  Created by Samuel Goodwin on 3/16/15.
+//  Copyright (c) 2015 Roundwall Software. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBAction func exit(segue: UIStoryboardSegue!) {}
+
+}
+
+class SecondViewController: UIViewController {
+    
+}
